8cf1e0e04a85ef0dc1d7473bc979c1e94c8a1560
ahadsenja <hamdanilm96@gmail.com>
2024-03-29 10:36:49 +0800

    update navbar wrapper styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,24 +3,26 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center h-16">
-      <h1 className='font-medium text-xl hover:text-accent hover:shadow-md'>
-        <Link href={'/'}>Next St</Link>
-      </h1>
-      <ul className='flex gap-8'>
-        <li className='hover:text-accent'>
-          <Link href={'/blog'}>Blog</Link>
-        </li>
-        <li className='hover:text-accent'>
-          <Link href={'/working-space'}>Working Space</Link>
-        </li>
-        <li className='hover:text-accent'>
-          <Link href={'/job-vacancy'}>Job Vacancy</Link>
-        </li>
-        <li className='hover:text-accent'>
-          <Link href={'/contact'}>Contact</Link>
-        </li>
-      </ul>
+    <div className="flex items-center h-16 sticky top-0 bg-white">
+      <div className='container mx-auto flex justify-between'>
+        <h1 className='font-medium text-xl hover:text-accent hover:shadow-md'>
+          <Link href={'/'}>Next St</Link>
+        </h1>
+        <ul className='flex gap-8'>
+          <li className='hover:text-accent'>
+            <Link href={'/blog'}>Blog</Link>
+          </li>
+          <li className='hover:text-accent'>
+            <Link href={'/working-space'}>Working Space</Link>
+          </li>
+          <li className='hover:text-accent'>
+            <Link href={'/job-vacancy'}>Job Vacancy</Link>
+          </li>
+          <li className='hover:text-accent'>
+            <Link href={'/contact'}>Contact</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
